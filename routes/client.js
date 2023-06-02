b8a6026531bf1a91b534f7dec29e0e84337ca134
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-06-02 09:55:16 +0700

    up

--- routes/client.js
+++ routes/client.js
@@ -1,11 +1,5 @@
-import express from 'express'
+import express from "express"
 
 const router = express.Router()
-
-router.get('/', (req,res)=>{
-    res.status(200).json({
-        message:'Hello World'
-    })
-})
 
 export default router
